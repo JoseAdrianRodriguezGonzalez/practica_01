efe9c196b5fecee0095244794ffa6158a92a73b8
JoseAdrianRodriguezGonzalez <kerpador@gmail.com>
2025-11-27 00:56:06 -0600

    ya quedo lo del arhcivo de texto generado con el intercalado

--- practica_01.py
+++ practica_01.py
@@ -1,4 +1,5 @@
 #Variables librerias globales
+from os import read
 import re
 import pandas as pd
 import nltk
@@ -14,7 +15,7 @@ def read_data(data_path:str)->dict[int,str]:
     dicc={}
     with open(data_path,"r",encoding="utf-8") as f:
         for idx,line in enumerate(f):
-            dicc[idx]=line
+            dicc[idx]=line.strip()
     return dicc
 def extraer_alfabeticos(diccionario:dict[int,str])->dict[int,list[str]]:
     diccionario_filtrado={}
@@ -42,13 +43,37 @@ def guardar_resultado(diccionario:dict[int,list[str]],nombre:str):
 Seccion 2.2 punto 2
 """
 ######################################
-#def frecuencias(topico:str):
-#    lista_de_palabras=topico.split(" ")
-#    diccionario_frecuencias={}
-#    for 
-#def palabras_frecuentes(diccionario:[int,str]):
-     
-
+def juntar_topicos(diccionario:dict[int,str],diccionario_labels:dict[int,str])->dict[str,list[str]]:
+    diccionario_vocabulario={}
+    for keys,items in diccionario.items():
+        diccionario_vocabulario[diccionario_labels[keys]]=diccionario_vocabulario.get(diccionario_labels[keys],"")+items
+    for keys in diccionario_vocabulario:
+        diccionario_vocabulario[keys]=diccionario_vocabulario[keys].split()
+    return diccionario_vocabulario
+def get_frecuencias(diccionario_completo:dict[str,list[str]])->dict[str,dict[str,int]]:
+    diccionario_por_topico={}
+    for keys,items in diccionario_completo.items():
+        diccionario_frecuencias={}
+        for palabra in items:
+            diccionario_frecuencias[palabra]=diccionario_frecuencias.get(palabra,0)+1
+        diccionario_por_topico[keys]=diccionario_frecuencias
+    return diccionario_por_topico
+def sort_frecuencias(diccionario_completo:dict[str,dict[str,int]])->dict[str,list[tuple[int,str]]]:
+    diccionario_topico={}
+    for keys,items in diccionario_completo.items():
+        diccionario_topico[keys]=sorted([(frequency,word) for word,frequency in items.items()],reverse=True)
+    return diccionario_topico
+def write_txt(diccionario:dict[str,list[tuple[int,str]]],cantidad:int):
+    n=cantidad//2
+    with open("keywords.txt","w") as f:
+        for keys,items in diccionario.items():
+            palabras_mas_frecuente=items[:cantidad]
+            print(palabras_mas_frecuente)
+            i=0
+            while(i<n):
+                f.write(f"{palabras_mas_frecuente[i]} {palabras_mas_frecuente[i+1]} {palabras_mas_frecuente[cantidad-1-i]} {palabras_mas_frecuente[cantidad-i-2]}\n")
+                i+=2
+        
 """
 Seccion 2.2 punto 1
 Llamada de funciones de limpieza de datos
@@ -59,14 +84,15 @@ documentos_limpios=extraer_alfabeticos(documentos)
 documento_sin_palabras_vacias=remover_palabras_vacias(documentos_limpios,stop_words)
 documentos_sin_acentos=quitar_acentos(documento_sin_palabras_vacias)
 guardar_resultado(documentos_sin_acentos,"data_processed.txt")
-print(" indice\tnorma\tfiltrado")
-for idx,valores in enumerate(zip(documentos.values(),documentos_sin_acentos.values())):
-    normal=valores[0]
-    filtrado=valores[1]
-    print(f"{idx}\t|{len(normal)}\t|{len(filtrado)}")
 """
 2.2 punto 2
 """
 data_processed="data_processed.txt"
+labels="labels.txt"
 diccionario_procesado=read_data(data_processed)
-
+diccionario_labels=read_data(labels)
+diccionario_labels={idx:items for idx,items in diccionario_labels.items() }
+diccionario_topicos=juntar_topicos(diccionario_procesado,diccionario_labels)
+diccionario_frecuencias=get_frecuencias(diccionario_topicos)
+diccionario_frecuencias_ordenadas=sort_frecuencias(diccionario_frecuencias)
+write_txt(diccionario_frecuencias_ordenadas,60)
